d37dcf48d46cd3a18e25c60a52900d305c55f36b
sijab <mar.jablonski96@wp.pl>
2021-12-05 19:54:38 +0100

    clean code

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,9 @@ import React, { useEffect } from 'react';
 import GlobalStyle from './GlobalStyle';
 import Navbar from './components/Navbar/Navbar'
 import AboutMe from './views/AboutMe/AboutMe';
-import { useAllMoviesData } from './hooks/useAllMoviesData';
 
 import { useDispatch } from 'react-redux'
-import { getAllMovies, getOneMovie } from './reducers/movies';
+import { getAllMovies } from './reducers/movies';
 
 const App = () => {
 
@@ -15,19 +14,11 @@ const App = () => {
     dispatch(getAllMovies())
   }, [])
 
-  const { data } = useAllMoviesData();
-
-  useEffect(() => {
-    console.log(data)
-  }, [data])
-
   return (
     <>
       <GlobalStyle />
       <Navbar />
       <AboutMe />
-      <button onClick={() => dispatch(getAllMovies())} >Get all</button>
-      <button onClick={() => dispatch(getOneMovie(1))} >one</button>
     </>
   );
 }
