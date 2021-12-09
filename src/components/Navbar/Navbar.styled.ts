64492b6016c3c0f5f63e2cd7ecf3f490b30bdcc4
sijab <mar.jablonski96@wp.pl>
2021-12-09 22:35:54 +0100

    fix navbar

--- src/components/Navbar/Navbar.styled.ts
+++ src/components/Navbar/Navbar.styled.ts
@@ -1,31 +1,20 @@
+import { motion } from "framer-motion";
 import styled from "styled-components";
 
 export const Container = styled.div`
   position: fixed;
-  width: 100%;
+  width: 100vw;
   height: 100px;
   font-family: 'Roboto', sans-serif;
   background: #1B1B1B;
   z-index: 2;
-`;
-
-export const ImgContainer = styled.div`
-  position: fixed;
-  right: 0;
-  overflow: hidden;
-  width: 25vw;
-  z-index: -1;
-`;
-
-export const Image = styled.img`
-  width: 100%;
+  display: flex;
+  justify-content: space-between;
 `;
 
 export const LogoContainer = styled.div`
   width: 20%;
   height: 100px;
-  position: absolute;
-  left: 0;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -35,8 +24,6 @@ export const LogoContainer = styled.div`
 `;
 
 export const RouterButtonContainer = styled.div`
-  position: absolute;
-  right: 0;
   width: 20%;
   height: 100px;
   display: flex;
@@ -54,4 +41,18 @@ export const RouterButton = styled.div<{btnColor: string}>`
   &:hover {
       border-bottom: 4px solid white ;
   }
+`;
+
+export const HamburgerContainer = styled.div`
+  width: 50px;
+  height: 50px;
+`;
+
+export const HamburgerLine = styled(motion.div)`
+  position: relative;
+  top: 25%;
+  width: 100%;
+  height: 5px;
+  background: yellow;
+  margin: 5px 0;
 `;
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import * as styled from './Navbar.styled';
 import blobsSvg from '../../assets/img/blobs.svg';
 import { config } from '../../config';
@@ -10,16 +10,39 @@ const Navbar: React.FC = () => {
   const { aboutMe, home } = config.routerPath;
   const navigate = useNavigate();
   const { mobile } = useIsMobile();
+  const [menuOpen, setMenuOpen] = useState<boolean>(false)
 
+  const lineOneVariants = {
+    initial: { rotate: "0deg" },
+    animate: { y: ".35rem", rotate: "45deg" },
+  };
+  const lineThreeVariants = {
+    initial: { rotate: "0deg" },
+    animate: { y: "-.35rem", rotate: "-45deg" },
+  };
 
   return (
     <>
       <styled.Container>
         <styled.LogoContainer>OK site</styled.LogoContainer>
-        <styled.RouterButtonContainer>
+        {!mobile ? <styled.RouterButtonContainer>
           <styled.RouterButton btnColor='yellow' onClick={() => navigate(home)} >Home</styled.RouterButton>
           <styled.RouterButton btnColor='white' onClick={() => navigate(aboutMe)} >About</styled.RouterButton>
-        </styled.RouterButtonContainer>
+        </styled.RouterButtonContainer> :
+        <styled.RouterButtonContainer>
+          <styled.HamburgerContainer onClick = {() => setMenuOpen(prev => !prev)}>
+            <styled.HamburgerLine
+              variants={lineOneVariants}
+              initial="initial"
+              animate={menuOpen ? "animate" : "initial"}
+            />
+            <styled.HamburgerLine
+              variants={lineThreeVariants}
+              initial="initial"
+              animate={menuOpen ? "animate" : "initial"}
+            />
+          </styled.HamburgerContainer>
+        </styled.RouterButtonContainer>}
       </styled.Container>
     </>
   )
