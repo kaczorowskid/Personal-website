57a2a1fe62ae7b6d3439c0a5e535fbd0cad8d0c3
sijab <mar.jablonski96@wp.pl>
2021-12-09 11:17:35 +0100

    formatted files to 2 spaces in a reducers folder

--- src/reducers/movies.ts
+++ src/reducers/movies.ts
@@ -5,46 +5,46 @@ import { callApi } from "../helper/callApi";
 const { moviesApiPath } = config.requestsPath
 
 export const getAllMovies = createAsyncThunk("movies/getAllMovies", async () => {
-    const { response, error } = await callApi(moviesApiPath)
-    if(error) console.log(error)
-    return response
+  const { response, error } = await callApi(moviesApiPath)
+  if (error) console.log(error)
+  return response
 });
 
 export const getOneMovie = createAsyncThunk('movies/getOneMovie', async (movieId: number) => {
-    const { response, error } = await callApi(`${moviesApiPath}/${movieId}`)
-    if(error) console.log(error)
-    return response
+  const { response, error } = await callApi(`${moviesApiPath}/${movieId}`)
+  if (error) console.log(error)
+  return response
 })
 
 interface Istate {
-    dataAll: any,
-    dataOne: any
+  dataAll: any,
+  dataOne: any
 }
 
 const initialState: Istate = {
-    dataAll: {},
-    dataOne: {}
+  dataAll: {},
+  dataOne: {}
 }
 
 const postsReducer = createSlice({
-    name: 'movies',
-    initialState: initialState,
-    reducers: {},
-    extraReducers: (builder: ActionReducerMapBuilder<Istate>) => {
-        builder.addCase(getAllMovies.fulfilled, (state: Istate, { payload }: PayloadAction<any>) => {
-            state.dataAll = payload
-            state.dataOne = {}
-        });
-        builder.addCase(getAllMovies.rejected, () => {
-            console.error("Any error to get all data");
-        });
-        builder.addCase(getOneMovie.fulfilled, (state: Istate, { payload }: PayloadAction<any>) => {
-            state.dataOne = payload
-        });
-        builder.addCase(getOneMovie.rejected, () => {
-            console.error("Any error to get one data");
-        });
-    }
+  name: 'movies',
+  initialState: initialState,
+  reducers: {},
+  extraReducers: (builder: ActionReducerMapBuilder<Istate>) => {
+    builder.addCase(getAllMovies.fulfilled, (state: Istate, { payload }: PayloadAction<any>) => {
+      state.dataAll = payload
+      state.dataOne = {}
+    });
+    builder.addCase(getAllMovies.rejected, () => {
+      console.error("Any error to get all data");
+    });
+    builder.addCase(getOneMovie.fulfilled, (state: Istate, { payload }: PayloadAction<any>) => {
+      state.dataOne = payload
+    });
+    builder.addCase(getOneMovie.rejected, () => {
+      console.error("Any error to get one data");
+    });
+  }
 })
 
 export default postsReducer.reducer;
