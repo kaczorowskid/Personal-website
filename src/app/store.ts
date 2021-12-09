9c7d9c8649cc8bf3373485c4cced5304d2376c98
sijab <mar.jablonski96@wp.pl>
2021-12-09 11:29:40 +0100

    formatted files to 2 spaces in a app folder

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,9 +2,9 @@ import { configureStore } from "@reduxjs/toolkit";
 import postsReducer from '../reducers/movies';
 
 export const store = configureStore({
-    reducer: {
-        movies: postsReducer
-    }
+  reducer: {
+    movies: postsReducer
+  }
 })
 
 export type RootState = ReturnType<typeof store.getState>
