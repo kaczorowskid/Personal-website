6a2a2e7d0e85bb262d6275887318965d5b5ede28
sijab <mar.jablonski96@wp.pl>
2021-12-09 11:17:03 +0100

    formatted files to 2 spaces in a routes folder

--- src/routes/AppRouting.tsx
+++ src/routes/AppRouting.tsx
@@ -3,20 +3,20 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { IRouting } from '../types/IRouting';
 
 interface Props {
-    routes: Array<IRouting>
+  routes: Array<IRouting>
 }
 
 const AppRouting: React.FC<Props> = ({ routes }) => {
 
-    return (
-        <Router>
-            <Routes>
-                {routes.map((route: IRouting, i: number) => (
-                    <Route key = {i} path = {route.path} element = {route.element} />
-                ))}
-            </Routes>
-        </Router>
-    )
+  return (
+    <Router>
+      <Routes>
+        {routes.map((route: IRouting, i: number) => (
+          <Route key={i} path={route.path} element={route.element} />
+        ))}
+      </Routes>
+    </Router>
+  )
 }
 
 export default AppRouting;
