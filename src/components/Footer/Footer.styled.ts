fabc58b4a295cbcd0738ea12215fce427257157e
sijab <mar.jablonski96@wp.pl>
2021-12-10 17:14:03 +0100

    Footer changes

--- src/components/Footer/Footer.styled.ts
+++ src/components/Footer/Footer.styled.ts
@@ -1,15 +1,16 @@
 import styled from "styled-components";
+import { colorConfig } from "../../colorsConfig";
 
 export const Container = styled.div`
   position: relative;
   width: 100%;
   height: 100px;
-  background: #1B1B1B;
+  background: ${colorConfig.lightBlack};
   display: flex;
   justify-content: center;
   align-items: center;
 `;
 
 export const Text = styled.span`
-  color: yellow;
+  color: ${colorConfig.yellow};
 `;
