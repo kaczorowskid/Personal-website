65f2b77b5a7ab8c87be2782d489864edd224c5d5
sijab <mar.jablonski96@wp.pl>
2021-12-10 18:05:33 +0100

    fix padding in end component

--- src/components/Home/End/End.styled.ts
+++ src/components/Home/End/End.styled.ts
@@ -107,7 +107,7 @@ export const CardContentContainer = styled.div`
   width: 100%;
   height: 100%;
   text-align: justify;
-  padding: 50px;
+  padding: 20px;
   display: flex;
   justify-content: center;
   align-items: center;
