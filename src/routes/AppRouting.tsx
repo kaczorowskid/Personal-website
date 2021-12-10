177d492c3aee759745c46288e875ebba2cc199d0
sijab <mar.jablonski96@wp.pl>
2021-12-10 14:32:39 +0100

    added ScrollToTop component - fix scrolling when routes change

--- src/routes/AppRouting.tsx
+++ src/routes/AppRouting.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, } from "react-router-dom";
 import { IRouting } from '../types/IRouting';
+import ScrollToTop from './ScrollToTop';
 
 interface Props {
   routes: Array<IRouting>
 }
 
 const AppRouting: React.FC<Props> = ({ routes }) => {
-
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         {routes.map((route: IRouting, i: number) => (
           <Route key={i} path={route.path} element={route.element} />
