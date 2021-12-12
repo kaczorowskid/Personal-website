456bc59f8482e65464c3961dd00d5c82e20f05a2
sijab <mar.jablonski96@wp.pl>
2021-12-12 20:20:31 +0100

    fix

--- src/components/AboutMe/StarWars/StarWars.tsx
+++ src/components/AboutMe/StarWars/StarWars.tsx
@@ -11,30 +11,32 @@ const StarWars: React.FC = () => {
   const navigate = useNavigate()
 
   return (
-    <styled.Container>
-      <styled.CardsContainer>
-        {data.length !== 0 ? data.map((item: any, i: number) => (
-          <styled.StarWarsCardContainer
-            onClick={() => navigate(`/movie/${urlParser(item.url)}`)}
-            initial={{ opacity: 0 }}
-            whileInView={{ opacity: 1 }}
-            viewport={{ once: true }}
-            transition={{
-              ...(i % 2 !== 0) && { delay: 0.5 },
-              duration: 1
-            }}
-            key={i} >
-            <styled.StarWarsHeaderConatiner>
-              <styled.StarWarsHeader>Star</styled.StarWarsHeader>
-              <styled.StarWarsHeader>Wars</styled.StarWarsHeader>
-            </styled.StarWarsHeaderConatiner>
-            <styled.TitleContainer>
-              <styled.MovieTitle>{item.title}</styled.MovieTitle>
-            </styled.TitleContainer>
-          </styled.StarWarsCardContainer>
-        )) : <ReactLoading type='spin' width={100} height={100} color='black' />}
-      </styled.CardsContainer>
-    </styled.Container>
+    <>
+      {data.length !== 0 && <styled.Container>
+        <styled.CardsContainer>
+          {data ? data.map((item: any, i: number) => (
+            <styled.StarWarsCardContainer
+              onClick={() => navigate(`/movie/${urlParser(item.url)}`)}
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1 }}
+              viewport={{ once: true }}
+              transition={{
+                ...(i % 2 !== 0) && { delay: 0.5 },
+                duration: 1
+              }}
+              key={i} >
+              <styled.StarWarsHeaderConatiner>
+                <styled.StarWarsHeader>Star</styled.StarWarsHeader>
+                <styled.StarWarsHeader>Wars</styled.StarWarsHeader>
+              </styled.StarWarsHeaderConatiner>
+              <styled.TitleContainer>
+                <styled.MovieTitle>{item.title}</styled.MovieTitle>
+              </styled.TitleContainer>
+            </styled.StarWarsCardContainer>
+          )) : <ReactLoading type='spin' width={100} height={100} color='black' />}
+        </styled.CardsContainer>
+      </styled.Container>}
+    </>
   )
 }
 
