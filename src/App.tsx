e3e1b16bce6f1e4cc3ed9e4e5c1299bcb48466ce
sijab <mar.jablonski96@wp.pl>
2021-12-22 21:52:59 +0100

    added routesMap

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,14 @@
 import React from 'react';
-
 import GlobalStyle from './GlobalStyle';
-import Home from './views/Home/Home';
-import AboutMe from './views/AboutMe/AboutMe';
-import StarWarsMovieInfo from './views/StarWarsMovieInfo/StarWarsMovieInfo';
-
-import { config } from './config';
 import AppRouting from './routes/AppRouting';
-import { IRouting } from './types/IRouting';
+import { routesMap } from './routes/routesMap';
 
 const App = () => {
-
-  const { home, aboutMe, movie } = config.routerPath;
-
-  const routes: Array<IRouting> = [
-    {
-      path: home,
-      element: <Home />
-    },
-    {
-      path: aboutMe,
-      element: <AboutMe />
-    },
-    {
-      path: movie,
-      element: <StarWarsMovieInfo />
-    }
-  ]
 
   return (
     <>
       <GlobalStyle />
-      <AppRouting routes={routes} />
+      <AppRouting routes={routesMap} />
     </>
   );
 }
---
+++ src/routes/routesMap.tsx
@@ -0,0 +1,22 @@
+import { IRouting } from "../types/IRouting";
+import AboutMe from "../views/AboutMe/AboutMe";
+import Home from "../views/Home/Home";
+import StarWarsMovieInfo from "../views/StarWarsMovieInfo/StarWarsMovieInfo";
+import { config } from "../config";
+
+const { home, aboutMe, movie} = config.routerPath;
+
+export const routesMap: Array<IRouting> = [
+    {
+      path: home,
+      element: <Home />
+    },
+    {
+      path: aboutMe,
+      element: <AboutMe />
+    },
+    {
+      path: movie,
+      element: <StarWarsMovieInfo />
+    }
+  ]
