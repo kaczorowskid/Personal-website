c537faaeac9d5757f3f7a0cf0363f711e4b0fdfd
sijab <kaczorowskid97@gmail.com>
2022-03-19 14:18:54 +0100

    removed unnecessary brackets

--- src/components/AboutMe/StarWars/StarWars.tsx
+++ src/components/AboutMe/StarWars/StarWars.tsx
@@ -11,32 +11,30 @@ const StarWars: React.FC = () => {
   const navigate = useNavigate()
 
   return (
-    <>
-      <styled.Container>
-        <styled.CardsContainer>
-          {!loading ? dataAllMovies.map((item: any, i: number) => (
-            <styled.StarWarsCardContainer
-              onClick={() => navigate(`/movie/${urlParser(item.url)}`)}
-              initial={{ opacity: 0 }}
-              whileInView={{ opacity: 1 }}
-              viewport={{ once: true }}
-              transition={{
-                ...(i % 2 !== 0) && { delay: 0.5 },
-                duration: 1
-              }}
-              key={i} >
-              <styled.StarWarsHeaderConatiner>
-                <styled.StarWarsHeader>Star</styled.StarWarsHeader>
-                <styled.StarWarsHeader>Wars</styled.StarWarsHeader>
-              </styled.StarWarsHeaderConatiner>
-              <styled.TitleContainer>
-                <styled.MovieTitle>{item.title}</styled.MovieTitle>
-              </styled.TitleContainer>
-            </styled.StarWarsCardContainer>
-          )) : <ReactLoading type='spin' width={100} height={100} color='black' />}
-        </styled.CardsContainer>
-      </styled.Container>
-    </>
+    <styled.Container>
+      <styled.CardsContainer>
+        {!loading ? dataAllMovies.map((item: any, i: number) => (
+          <styled.StarWarsCardContainer
+            onClick={() => navigate(`/movie/${urlParser(item.url)}`)}
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            viewport={{ once: true }}
+            transition={{
+              ...(i % 2 !== 0) && { delay: 0.5 },
+              duration: 1
+            }}
+            key={i} >
+            <styled.StarWarsHeaderConatiner>
+              <styled.StarWarsHeader>Star</styled.StarWarsHeader>
+              <styled.StarWarsHeader>Wars</styled.StarWarsHeader>
+            </styled.StarWarsHeaderConatiner>
+            <styled.TitleContainer>
+              <styled.MovieTitle>{item.title}</styled.MovieTitle>
+            </styled.TitleContainer>
+          </styled.StarWarsCardContainer>
+        )) : <ReactLoading type='spin' width={100} height={100} color='black' />}
+      </styled.CardsContainer>
+    </styled.Container>
   )
 }
 
--- src/components/Home/ScrollDownIcon/ScrollDownIcon.tsx
+++ src/components/Home/ScrollDownIcon/ScrollDownIcon.tsx
@@ -3,16 +3,14 @@ import * as styled from './ScrollDownIcon.styled';
 
 const ScrollDownIcon: React.FC = () => {
   return (
-    <>
-      <styled.Wrapper>
-        <styled.ScrollDown>
-          <styled.SVGContainer height="30" width="10">
-            <styled.ScrollDownPoint cx="5" cy="15" r="3" />
-            <styled.ScrollDownPoint isSecond={true} cx="5" cy="15" r="3" />
-          </styled.SVGContainer>
-        </styled.ScrollDown>
-      </styled.Wrapper>
-    </>
+    <styled.Wrapper>
+      <styled.ScrollDown>
+        <styled.SVGContainer height="30" width="10">
+          <styled.ScrollDownPoint cx="5" cy="15" r="3" />
+          <styled.ScrollDownPoint isSecond={true} cx="5" cy="15" r="3" />
+        </styled.SVGContainer>
+      </styled.ScrollDown>
+    </styled.Wrapper>
   )
 }
 
